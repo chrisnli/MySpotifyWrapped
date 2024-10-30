e345612228bea9f6ee52aad793263316624b7541
emerritt8 <emerritt8@gatech.edu>
2024-10-30 13:48:32 -0400

    Added enough documentation to what we've already written to pass all lints

--- spotify_wrapped/models.py
+++ spotify_wrapped/models.py
@@ -1,5 +1,15 @@
+"""
+Database models and associated functions, which allow our web app to
+effectively communicate with our database backend.
+"""
+
 from django.db import models
 from django.contrib.auth.models import User
 
 class Account(models.Model):
+    """
+    A representation of a user's account, which contains a one-to-one
+    relationship with a Django User. Allows us to extend Django users with
+    additional fields.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
--- spotify_wrapped/views.py
+++ spotify_wrapped/views.py
@@ -1,3 +1,7 @@
+"""
+Logic required to display correct web pages to users of our web app.
+"""
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
@@ -6,17 +10,30 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 def index(request):
+    """
+    :param request: The http request received
+    :return: A HttpResponse for the index of our web app
+    """
     return render(request, "SpotifyWrapped/homepage.html")
 
 def account_creation(request):
+    """
+    If the request uses the POST method with correct user creation data,
+    creates a new user in the database and logs the new user in, sending them
+    to the website home page. In other cases, it will redirect them to the
+    signup page, displaying any errors with their previous account creation
+    attempt if there are any.
+    :param request: The http request received
+    :return: A HttpResponse for our homepage if successful, or for the account
+    creation page otherwise
+    """
     if request.method != 'POST':
         return render(request, "registration/register.html", {"form":UserCreationForm})
-    else:
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
-            user.save()
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "registration/register.html", {"form": form})
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        user = User.objects.create_user(username=form.cleaned_data['username'],
+                                        password=form.cleaned_data['password1'])
+        user.save()
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    return render(request, "registration/register.html", {"form": form})
