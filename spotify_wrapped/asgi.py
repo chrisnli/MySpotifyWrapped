59468c3bcc9fe659f20929e7fe7641f9333fbe4f
emerritt8 <emerritt8@gatech.edu>
2024-11-01 13:31:13 -0400

    Changed asgi.py and wsgi.py to use the settings module with the new module name

--- spotify_wrapped/asgi.py
+++ spotify_wrapped/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpotifyWrapped.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotify_wrapped.settings')
 
 application = get_asgi_application()
