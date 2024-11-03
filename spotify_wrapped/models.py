b1165d0c58f70c31fdd6a312feab417cd4654da9
emerritt8 <emerritt8@gatech.edu>
2024-11-02 23:19:13 -0400

    Updated the Account model to hold access tokens, access token expiration time, and refresh tokens. Also gave it methods to return the access token, refreshing it as necessary

--- spotify_wrapped/models.py
+++ spotify_wrapped/models.py
@@ -3,8 +3,11 @@ Database models and associated functions, which allow our web app to
 effectively communicate with our database backend.
 """
 
+from datetime import timedelta
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
+import requests
 
 User = get_user_model()
 
@@ -15,6 +18,9 @@ class Account(models.Model):
     additional fields.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    access_token = models.TextField(null=True)
+    access_token_expiration_time = models.DateTimeField(null=True)
+    refresh_token = models.TextField(null=True)
 
     @classmethod
     def new(cls, username, password):
@@ -31,3 +37,40 @@ class Account(models.Model):
         account = Account.objects.create(user=user)
         account.save()
         return account
+
+    def access_expired(self):
+        """
+        :return: Whether the stored access token has expired
+        """
+        return timezone.now() > self.access_token_expiration_time
+
+    def get_valid_access_token(self):
+        """
+        :return: The stored access token if it has not expired, otherwise
+        refreshes the stored access token and returns that
+        """
+        if self.access_expired():
+            self.refresh()
+        return self.access_token
+
+    def refresh(self):
+        """
+        Uses the stored refresh token to query a new access token, then
+        stores the access token and the access token's expiration time
+        """
+        url = 'https://accounts.spotify.com/api/token'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': self.refresh_token,
+            # IMPORTANT: once secrets.py is in main, change this from being
+            # just a string to being actual code
+            'client_id': 'secrets.get_cli_id()',
+        }
+        response = requests.post(url, headers=headers, data=body, timeout=5)
+        response_json = response.json()
+        self.access_token = response_json['access_token']
+        self.access_token_expiration_time = timezone.now() + timedelta(
+            seconds=response_json['expires_in'])
